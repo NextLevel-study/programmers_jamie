06fbc98e5b68b592b6d58938b9b8c81aac7243da
JamieShin0201 <jamieshin1450@gmail.com>
2021-10-07 00:13:55 +0900

    서울에서 김서방 찾기 리팩토링

--- src/main/java/level1/서울에서김서방찾기.java
+++ src/main/java/level1/서울에서김서방찾기.java
@@ -3,12 +3,17 @@ package level1;
 public class 서울에서김서방찾기 {
 
     public String solution(String[] seoul) {
+        int kimLocation = findKimLocation(seoul);
+        return "김서방은 " + kimLocation + "에 있다";
+    }
+
+    public int findKimLocation(String[] seoul) {
         for (int i = 0; i < seoul.length; i++) {
             if (seoul[i].equals("Kim")) {
-                return "김서방은 " + i + "에 있다";
+                return i;
             }
         }
-
-        return "";
+        
+        return -1;
     }
 }
--- src/test/java/서울에서김서방찾기Test.java
+++ src/test/java/서울에서김서방찾기Test.java
@@ -5,11 +5,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class 서울에서김서방찾기Test {
 
+    서울에서김서방찾기 cls = new 서울에서김서방찾기();
+
     @Test
     void solution() {
         String[] seoul = {"Jane", "Kim"};
-        서울에서김서방찾기 cls = new 서울에서김서방찾기();
 
         assertEquals("김서방은 1에 있다", cls.solution(seoul));
+    }
+
+    @Test
+    void findKimLocation() {
+        String[] seoul = {"Kim", "Jane"};
+        String[] seoul1 = {"Jane", "Kim"};
+        String[] seoul2 = {"Someone", "Jane", "Kim"};
+
+        assertEquals(0, cls.findKimLocation(seoul));
+        assertEquals(1, cls.findKimLocation(seoul1));
+        assertEquals(2, cls.findKimLocation(seoul2));
     }
 }
